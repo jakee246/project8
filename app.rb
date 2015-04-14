414ff8a43a07d1a8dc6b964344222f9df8abaf41
Jakee246 <Jake@14-66-53.guest.wireless.msu.edu>
2015-04-14 15:08:14 -0400

    Indentation fix

--- app.rb
+++ app.rb
@@ -4,17 +4,17 @@ require 'date'
 
 class MyWebApp < Sinatra::Base
   get '/' do
-  	country = params['country'] || 'us'
+    country = params['country'] || 'us'
 
-  	if params['month'] != nil
+    if params['month'] != nil
       string = params['month']
       string += '-01'
       date = Date.parse(string)
-  	else
-  		date = Date.new
-  	end
+    else
+      date = Date.new
+    end
 
-  	@holidays = HolidApi.get(country: country, month: date.month, year: date.year)
-  	erb :index
+    @holidays = HolidApi.get(country: country, month: date.month, year: date.year)
+    erb :index
   end
 end
